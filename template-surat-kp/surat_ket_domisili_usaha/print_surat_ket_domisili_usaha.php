1cf90fcd2415bdcd638ea30c9340ceea97a8fcef
Eko Bayu <ekobayu99@gmail.com>
2023-03-27 10:05:52 +0700

    perbaikan template surat ket domisili usaha

--- template-surat-kp/surat_ket_domisili_usaha/print_surat_ket_domisili_usaha.php
+++ template-surat-kp/surat_ket_domisili_usaha/print_surat_ket_domisili_usaha.php
@@ -54,7 +54,7 @@
 			<tr>
 				<td style="vertical-align: top">Alamat / Tempat Tinggal</td>
 				<td style="vertical-align: top">:</td>
-				<td style="vertical-align: top">RT. <?= $data['no_rt'] ?>, RW. <?= $data['no_rw'] ?>, Pedukuhan <?= ununderscore(unpenetration($data['alamat_sekarang'])) ?>, <?= ucwords($this->setting->sebutan_desa) ?> <?= unpenetration($desa['nama_desa']) ?>, <?= ucwords($this->setting->sebutan_kecamatan_singkat) ?> <?= strtoupper(unpenetration($desa['nama_kecamatan'])) ?>, <?= ucwords($this->setting->sebutan_kabupaten_singkat) ?> <?= unpenetration($desa['nama_kabupaten']) ?></td>
+				<td style="vertical-align: top">RT. <?= $data['no_rt'] ?>, RW. <?= $data['no_rw'] ?>, Pedukuhan <?= underscore(unpenetration($data['alamat_sekarang'])) ?>, <?= ucwords($this->setting->sebutan_desa) ?> <?= unpenetration($desa['nama_desa']) ?>, <?= ucwords($this->setting->sebutan_kecamatan_singkat) ?> <?= strtoupper(unpenetration($desa['nama_kecamatan'])) ?>, <?= ucwords($this->setting->sebutan_kabupaten_singkat) ?> <?= unpenetration($desa['nama_kabupaten']) ?></td>
 			</tr>
 			<tr>
 				<td>Agama</td>
@@ -86,7 +86,7 @@
 
 	<table>
 		<tr>
-			<td>Bahwa yang tersebut namanya diatas, memang benar penduduk kami yang memiliki usaha <?= $log_surat_detil['usaha'] ?>, berlokasi di Dusun <?= ununderscore(unpenetration($data['alamat_sekarang'])) ?>, RT. <?= $data['no_rt'] ?>, <?= unpenetration($data['alamat_sekarang']) ?>, <?= unpenetration($desa['nama_kecamatan']) ?>, <?= unpenetration($desa['nama_kabupaten']) ?>.</td>
+			<td>Bahwa yang tersebut namanya diatas, memang benar penduduk kami yang memiliki usaha <?= $log_surat_detil['usaha'] ?>, berlokasi di Dusun <?= underscore(unpenetration($data['alamat_sekarang'])) ?>, RT. <?= $data['no_rt'] ?>, <?= unpenetration($data['alamat_sekarang']) ?>, <?= unpenetration($desa['nama_kecamatan']) ?>, <?= unpenetration($desa['nama_kabupaten']) ?>.</td>
 		</tr>
 	</table>
 
